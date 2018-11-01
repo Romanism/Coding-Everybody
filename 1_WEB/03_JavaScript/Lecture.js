3d1078cef6eea291eb90283b77d94c381dc0d932
JISUNGIN <jsi920629@gmail.com>
2018-11-01 18:32:26 +0900

    Add: JavaScript ~8강까지

---
+++ 1_WEB/03_JavaScript/Lecture.js
@@ -0,0 +1,15 @@
+/* 
+JavaScript는 왜 생겨났을까?
+- 사용자와 상호작용하는 웹페이지를 만들고 싶다는 욕망때문입니다. (html은 정적 타입, Javascript는 동적 타입)
+- HTML로 웹을 구성한 다음에 JavaScript를 통해 사용자와 상호작용을 할 수 있게됩니다.
+- HTML은 만화, JavaScript는 애니메이션 느낌
+*/
+
+
+/* 
+어떻게 HTML과 JavaScript는 상호작용 할 수 있을까??
+
+1. HTML에 <script> 태그를 넣어 만들 수 있습니다.
+2. HTML 태그에 JavaScript를 사용할 수 있는 이벤트를 사용합니다. ex) onclick, onchanged
+3. 웹에 있는 Console을 이용합니다. (JavaScript 문법 사용 가능)
+*/
